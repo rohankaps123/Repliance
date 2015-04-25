8c8e7d17c9bac94d2986fec7764f0768fa9dd089
jfdesjar <jfdesjar@umass.edu>
2015-04-25 16:15:05 +0000

    my answers working rough draft

--- repliance/routes/myanswers.js
+++ repliance/routes/myanswers.js
@@ -9,14 +9,23 @@ var online = users.online;
 router.get('/', function(req, res) {
 
   var user = req.session.user;
-  var username = user.username;
-  if (user === undefined || online[user.uid] === undefined) {
-    req.flash('auth', 'Not logged in!');
-    res.redirect('/user/login');
-  }
-  else {
-  res.render('myanswers', { title : 'My Answers', username: username});
-  }
-});
+	  if (user === undefined || online[user.uid] === undefined) {
+	    req.flash('auth', 'Not logged in!');
+	    res.redirect('/user/login');
+	  }
+	  else {
+	  	dblib.userAns(user, function(error, data){
+	  		if(user === undefined || online[user.uid] === undefined){
+	  			req.flash('auth', 'Not logged in!');
+	    		res.redirect('/user/login');
+	  		}
+	  		else{
+	  			res.render('myanswers', {title : 'My Answers',
+										  username : user.username,
+										  data : data });
+	  		}
+	  	});
+	  }
+  });
 
 module.exports = router;
