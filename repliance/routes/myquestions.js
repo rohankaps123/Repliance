3720a91c2d5ec91bbebaa365f6b03de76c0a1c43
Kalina Nedkova <knedkova@umass.edu>
2015-04-25 11:40:47 -0400

    display my questions

--- repliance/routes/myquestions.js
+++ repliance/routes/myquestions.js
@@ -20,38 +20,11 @@ router.get('/', function(req, res) {
 	    		res.redirect('/user/login');
 	  		}
 	  		else{
-	  			/*
-				var htmlString = '<ul class = "questionList"> ';
-	  			console.log(data);
-	  			console.log(data.rows.length);
-	  			for(var i = 0; i < data.rows.length; i++){
-	  				var qid = data.rows[i].qid;
-	  				var repliesTotal = data.rows[i].repliestotal;
-	  				var repliesLimit = data.rows[i].replieslimit;
-	  				var timeTotal = data.rows[i].timetotal;
-	  				var timeLimit = data.rows[i].timelimit;
-	  				var bodyText = data.rows[i].bodytext;
-	  				var title = data.rows[i].title;
-
-	  				htmlString = htmlString + '<li class = "questionElement"> <div class = "questionBox">' + 
-	  											'<p class = "questTitle">' + title + '</p>' +
-	  											'<p class = "questBody">' + bodyText + '</p>' +
-	  											'<p class = "questRepLeft">' + repliesTotal + '</p>' +
-	  											'<p class = "questRepLim">' + repliesLimit + '</p>' +
-	  											'</div></li>';
-	  			}
-	  			htmlString = htmlString + '</ul>';
-	  			*/
-				
-				
 	  			res.render('myquestions', {title : 'My Questions',
 										  username : user.username,
-										  data : data
-	  									   //myquestions : htmlString
-										   });
+										  data : data });
 	  		}
 	  	});
-	  	//res.render('myquestions', { title : 'My Questions'});
 	  }
   });
 
